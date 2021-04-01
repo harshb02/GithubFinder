788fbbbca9290968781efe23413d6272ee8b60c3
harshb02 <harsh.ma02@gmail.com>
2021-04-01 14:29:42 +0530

    Updated the app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import axios from 'axios';
 import About from './components/pages/About';
 import './App.css';
 
+const github = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN },
+})
+
 class App extends Component {
   state = {
     users: [],
@@ -19,37 +24,33 @@ class App extends Component {
   }
 
   // Search Github Users
-  searchUsers = async text => {
-    this.setState({loading: true});
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-      );
-
-      this.setState({ users: res.data.items, loading: false });
+    searchUsers = async (text) => {
+    this.setState({ loading: true })
+ 
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
+ 
+    this.setState({ users: res.data.items, loading: false })
   }
 
   // Get single Github user
-  getUser =  async username => {
-    this.setState({loading: true});
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-      );
-
-    this.setState({users: res.data.items, loading:false});
-  } 
+  getUser = async (username) => {
+    this.setState({ loading: true })
+ 
+    const res = await axios.get(`https://api.github.com/users/${username}`)
+ 
+    this.setState({ users: res.data.items, loading: false })
+  }
 
   // Get users repos
-  getUserRepos =  async username => {
-    this.setState({loading: true});
-
+  getUserRepos = async (username) => {
+    this.setState({ loading: true })
+ 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-      );
-
-    this.setState({repos: res.data.items, loading:false});
-  } 
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`
+    )
+ 
+    this.setState({ repos: res.data.items, loading: false })
+  }
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading:false});
